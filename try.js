fc2b60880a10cc5b835b413836c5de95555d4f8b
rishabh khanna <rishabhkhanna777@gmail.com>
2017-05-01 20:09:50 +0000

    script to move in one direction
    Signed-off-by: rishabh khanna <rishabhkhanna777@gmail.com>

--- try.js
+++ try.js
@@ -4,8 +4,19 @@
 const gpio = require('onoff').Gpio;
 const sleep = require('sleep');
 var pin17  = new gpio(17, 'out');
+var pin22 = new gpio(22,'out');
 
-pin17.writeSync(1);
+pin17.write(1,function(){
+console.log("true on pin 17")
+});
+pin22.write(1,function(){
+console.log("true on pin 22");
+});
 sleep.msleep(5000);
-pin17.writeSync(0);
+pin17.write(0,function(){
+console.log("false on pin 17")
+});
+pin22.write(0,function(){
+console.log("false on pin 22")
+})
 
